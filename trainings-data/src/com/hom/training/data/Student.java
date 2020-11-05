8ea8b2cb12b3c3e6889fcc69cfc97035c4b51d3c
a1 <EvelinaTubalec>
2020-11-05 23:31:57 +0300

    data add

---
+++ trainings-data/src/com/hom/training/data/Student.java
@@ -0,0 +1,14 @@
+package com.hom.training.data;
+
+public class Student {
+
+    private String firstName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public Student(String firstName) {
+        this.firstName = firstName;
+    }
+}
