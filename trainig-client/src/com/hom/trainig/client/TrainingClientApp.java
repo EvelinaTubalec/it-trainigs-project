79882cc77e2c9a6d83fdce10de8fbdcb1cdf796d
a1 <EvelinaTubalec>
2020-11-05 23:42:25 +0300

    import student to server

--- trainig-client/src/com/hom/trainig/client/TrainingClientApp.java
+++ trainig-client/src/com/hom/trainig/client/TrainingClientApp.java
@@ -1,7 +1,11 @@
 package com.hom.trainig.client;
 
+import com.hom.training.data.Student;
+
 public class TrainingClientApp {
     public static void main(String[] args) {
         System.out.println("Client app start");
+
+        Student student = new Student("Ivan");
     }
 }
