0aa98e94c17d6c445021d1198a509971c89de44c
a1 <EvelinaTubalec>
2020-11-05 23:50:24 +0300

    Revert "import student to client"
    This reverts commit 72aad238

--- trainings-server/src/com/home/trainig/server/TrainingServerApp.java
+++ trainings-server/src/com/home/trainig/server/TrainingServerApp.java
@@ -1,12 +1,9 @@
 package com.home.trainig.server;
 
-import com.hom.training.data.Student;
-
 public class TrainingServerApp {
 
     public static void main(String[] args) {
         System.out.println("Server app start");
-        Student student = new Student("Pavel");
     }
 }
 
